329d40a509a460ed108ec25cfc92dbd0deeb977f
ladiladi <38681465+ladiladi@users.noreply.github.com>
2020-08-13 13:18:52 +0300

    Updates

---
+++ precache-manifest.079faf258bd85c961a44ec7139f45b74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1eb8a083bfbd7652a5ab16e8f79a6c3",
+    "url": "/chatbot/index.html"
+  },
+  {
+    "revision": "c3c0d08530b56bee058f",
+    "url": "/chatbot/static/css/main.c7ba49c4.chunk.css"
+  },
+  {
+    "revision": "72a9a46ad4b8206747b2",
+    "url": "/chatbot/static/js/2.a52589b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chatbot/static/js/2.a52589b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3c0d08530b56bee058f",
+    "url": "/chatbot/static/js/main.c83a492f.chunk.js"
+  },
+  {
+    "revision": "6da786bfa225ae5459b9",
+    "url": "/chatbot/static/js/runtime-main.253fb56a.js"
+  }
+]);
